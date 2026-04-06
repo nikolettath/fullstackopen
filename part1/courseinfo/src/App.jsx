4f7198e4f52b79d9cbfa271a9bbaacfcf5492cee
Nikoletta <p3230055@aueb.gr>
2026-04-07 00:21:56 +0300

    part1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,46 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Header = ({course}) => {
+     return <div><h1>{course}</h1></div>
+  }
+
+  const Part = ({parts, exercises}) => {
+    return <div>
+      <p>{parts} {exercises}</p>
+    </div>
+  }
+
+  const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
+    return <div>
+      <Part parts={part1} exercises={exercises1}/>
+      <Part parts={part2} exercises={exercises2}/>
+      <Part parts={part3} exercises={exercises3}/>
+    </div>
+
+  }
+
+  const Total = ({exercises1, exercises2, exercises3}) => {
+    return <div>
+      Number of exercises {exercises1 + exercises2 + exercises3}
+    </div>
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} 
+        part2={part2} exercises2={exercises2} 
+        part3={part3} exercises3={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}  />
+    </div>
+  )
+}
+
+export default App
